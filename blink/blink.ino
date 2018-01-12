16f41cb51b53269fc2b4703765a886662862bfc4
MrGobus <mrgobus@gmail.com>
2018-01-12 19:22:49 +0300

    commit

--- blink/blink.ino
+++ blink/blink.ino
@@ -2,18 +2,24 @@
  * Пины встроенных светодиодов
  * Значения отличаются от стандартной arduino
  */
- 
+
 #define RX_LED_PIN 17 // Пин встроенного светодиода RX
 #define TX_LED_PIN 30 // Пин встроенного светодиода TX
 #define EXT_LED_PIN 3 // Пин внешнего светодиода
 
-#define LED_ON 0
-#define LED_OFF 1
+/*
+ * По какой то, видимо технологической, причине включение
+ * и выключение встроенных светодиодов инвертировано, по этому определяем
+ * константы для крастоы кода.
+ */
+
+#define INNER_LED_ON 0
+#define INNER_LED_OFF 1
 
 /*
  * Инициализация.
  */
- 
+
 void setup() {
   // Устанавливаем режим вывода для светодиодов
   pinMode(RX_LED_PIN, OUTPUT);
@@ -26,15 +32,17 @@ void setup() {
  */
 
 void loop() {
-  // Последовательно включаем встроенные светодиды 
+  // Последовательно включаем встроенные светодиоды
+  // и моргаем внешним светодиодом
   // с задержкой в 1000 миллисекунд (1 секунда)
-  digitalWrite(EXT_LED_PIN, HIGH);
-  digitalWrite(RX_LED_PIN, LED_ON);
-  digitalWrite(TX_LED_PIN, LED_OFF);
-  delay(1000);
-  digitalWrite(EXT_LED_PIN, LOW);
-  digitalWrite(RX_LED_PIN, LED_OFF);
-  digitalWrite(TX_LED_PIN, LED_ON);
-  delay(1000);
-}
 
+  digitalWrite(EXT_LED_PIN, HIGH); // Включает внешний светодиод
+  digitalWrite(RX_LED_PIN, INNER_LED_ON); // Включает внутренний светодиод RX
+  digitalWrite(TX_LED_PIN, INNER_LED_OFF); // Выключает внутренний светодиод TX
+  delay(1000); // Пауза
+
+  digitalWrite(EXT_LED_PIN, LOW); // Выключает внешний светодиод
+  digitalWrite(RX_LED_PIN, INNER_LED_OFF); // Выключает внутренний светодиод RX
+  digitalWrite(TX_LED_PIN, INNER_LED_ON); // Включает внутренний светодиод TX
+  delay(1000); // Пауза
+}
