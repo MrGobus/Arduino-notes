2ebc7616f77c4add50e06174ab09ddc1a8615426
MrGobus <mrgobus@gmail.com>
2018-01-12 11:17:08 +0300

    commit

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,9 +1,22 @@
 /*
+ * Пины встроенных светодиодов
+ * Значения отличаются от стандартной arduino
+ */
+ 
+#define RX_LED_PIN 17 // Пин встроенного светодиода RX
+#define TX_LED_PIN 30 // Пин встроенного светодиода TX
+
+#define LED_ON 0
+#define LED_OFF 1
+
+/*
  * Инициализация.
  */
  
 void setup() {
-  
+  // Устанавливаем режим вывода для светодиодов
+  pinMode(RX_LED_PIN, OUTPUT);
+  pinMode(TX_LED_PIN, OUTPUT);  
 }
 
 /*
@@ -11,6 +24,13 @@ void setup() {
  */
 
 void loop() {
-  
+  // Последовательно включаем встроенные светодиды 
+  // с задержкой в 1000 миллисекунд (1 секунда)
+  digitalWrite(RX_LED_PIN, LED_ON);
+  digitalWrite(TX_LED_PIN, LED_OFF);
+  delay(1000);
+  digitalWrite(RX_LED_PIN, LED_OFF);
+  digitalWrite(TX_LED_PIN, LED_ON);
+  delay(1000);
 }
 
