0a34e7cdc8468ca125ddec039e4a3dccfd2c00ca
MrGobus <mrgobus@gmail.com>
2018-01-13 17:31:37 +0300

    commit

--- button/button.ino
+++ button/button.ino
@@ -1,10 +1,17 @@
-#define BUTTON_PIN 2
-#define TX_LED_PIN 30
+/**
+ * Включаем встроенный светодиод при нажатии на кнопку
+ *
+ * @platform Arduino Pro Micro (Leonardo)
+ * @author: MrGobus <mrgobus@gmail.com>
+ */
 
-int ledState;
+#define BUTTON_PIN 2 // Пин подключенной кнопки
+#define TX_LED_PIN 30 // Пин встроенного светодиода
+
+int ledState; // Состояние светодиода
 
 void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); //чтение с использованием внутреннего резистора
   pinMode(TX_LED_PIN, OUTPUT);
   ledState = digitalRead(BUTTON_PIN);
 }
@@ -13,6 +20,10 @@ void loop() {
   int buttonState = digitalRead(BUTTON_PIN);
   if (ledState != buttonState) {
     ledState = buttonState;
+	// Изменяем состояние светодиода
+	// Так как значения для внутренних светодиодов должно быть инвертировано
+	// вместо !ledState посылаем ledState
+	// Особенность платы
     digitalWrite(TX_LED_PIN, ledState);
   }
 }
